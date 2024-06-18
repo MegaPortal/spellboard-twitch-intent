1c1cc9b7c613c642e278238165657eae4d89f022
jo32 <j.jolam@gmail.com>
2024-06-18 23:06:13 +0800

    chore: Refactor login route to use Next.js API route format

--- src/pages/api/auth/login.tsx
+++ src/pages/api/auth/login.tsx
@@ -1,7 +1,6 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import jwt from '@tsndr/cloudflare-worker-jwt';
 import { aesEncrypt } from '@/lib/encrypt';
-
-export const dynamic = "force-dynamic";
 
 const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID;
 const TWITCH_CLIENT_SECRET = process.env.TWITCH_SECRET;
@@ -9,15 +8,17 @@ const TWITCH_ACCESS_TOKEN_ENDPOINT = 'https://id.twitch.tv/oauth2/token';
 const JWT_SECRET = process.env.JWT_SECRET;
 
 // logic for login, setting session content to cookie
-export async function GET(req: Request) {
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+
+    if (req.method !== 'GET') {
+        return res.status(405).json({ error: 'Method Not Allowed' });
+    }
 
     if (!TWITCH_CLIENT_ID || !TWITCH_CLIENT_SECRET || !JWT_SECRET) {
-        return new Response(JSON.stringify({ error: 'env is not set' }), {
-            status: 500,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
+        return res.status(500).json({ error: 'env is not set' });
     }
 
     const tokenResponse = await fetch(TWITCH_ACCESS_TOKEN_ENDPOINT, {
@@ -38,19 +39,13 @@ export async function GET(req: Request) {
     const maxExpiresIn = 60 * 60;
     const expiresIn = Math.min(bodyExpiresIn, maxExpiresIn);
 
-
     // encrypt the body into jwt token
     const loginInfo = await jwt.sign(body, JWT_SECRET);
     const loginInfoBuffer = await aesEncrypt(loginInfo, JWT_SECRET);
 
-    return new Response(JSON.stringify({
+    res.setHeader('Set-Cookie', `twitch_session=${encodeURIComponent(loginInfoBuffer)}; HttpOnly; Secure; SameSite=None; Path=/; Max-Age=${expiresIn}`);
+    return res.status(200).json({
         expiresIn,
         session: loginInfoBuffer,
-    }), {
-        status: 200,
-        headers: {
-            'Content-Type': 'application/json',
-            'Set-Cookie': `twitch_session=${encodeURIComponent(loginInfoBuffer)}; HttpOnly; Secure; SameSite=None; Path=/; Max-Age=${expiresIn}`,
-        },
-    })
+    });
 }
